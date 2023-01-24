48193496e4299e76dc3260d253bde30f7aca9c76
conner0714 <csatawidinata@gmail.com>
2023-01-24 12:40:22 -0800

    First person

--- Fighting Game/Assets/Scripts/MoveCamera.cs
+++ Fighting Game/Assets/Scripts/MoveCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
 
